353753f1ce4499dbcf995e4fb70a5a233b21d427
Wanglin337 <bayboyrt@gmail.com>
2025-07-09 13:51:28 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Root di sini artinya Vite mencari index.html di direktori utama
-  build: {
-    outDir: 'dist',
-  },
-});
+})
